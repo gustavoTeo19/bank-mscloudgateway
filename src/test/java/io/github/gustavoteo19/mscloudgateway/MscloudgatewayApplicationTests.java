2467e938d39f8de1b53e328368f28d2b2de8ce2d
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-12-17 14:24:38 -0300

    first commit

---
+++ src/test/java/io/github/gustavoteo19/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavoteo19.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
